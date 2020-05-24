cd223b8faaf6adb1d04a616cce2d6c8404697cf0
Jorge Dumont <jdumontl@hotmail.com>
2020-05-24 18:33:53 +0200

    mapas perfeccionados

--- src/app/tabHomeConductor/tabHomeConductor.page.ts
+++ src/app/tabHomeConductor/tabHomeConductor.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Map, tileLayer, marker, polyline } from "leaflet";
-import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { Map, tileLayer, marker, circle, icon} from "leaflet";
+
 
 
 @Component({
@@ -11,43 +11,38 @@ import { Geolocation } from "@ionic-native/geolocation/ngx";
 export class Tab2Page {
   map: Map;
   marker: any;
-  latLong = [];
+  icon: any;
+  circle: any;
+  greenIcon = icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
-  constructor(private geolocation: Geolocation) {}
+  constructor() {}
 
   ionViewDidEnter(){
     this.showMap();
   }
 
   showMap() {
-
     this.map = new Map("myMap").setView([40.410915, -3.708794], 55);
     tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
 
+    this.marker = marker([40.410915, -3.708794], 25).addTo(this.map).bindPopup("I am here.");;
+    this.circle = circle([40.410915, -3.708794], {
+      color: 'blue',
+      fillOpacity: 0.2,
+      radius: 75
+  }).addTo(this.map);
+
+  this.marker = marker([40.410415, -3.708794], {icon: this.greenIcon}).addTo(this.map);
+  this.marker = marker([40.410915, -3.708294], {icon: this.greenIcon}).addTo(this.map);
+  this.marker = marker([40.410215, -3.708794], {icon: this.greenIcon}).addTo(this.map);
+
   }
 
-  getPositions(){
-    this.geolocation.getCurrentPosition({
-      enableHighAccuracy: true
-    }).then((res) => {
-      return this.latLong = [
-        res.coords.latitude,
-        res.coords.longitude
-      ]
-    }).then((latlng) => {
-      if (this.marker) {
-        this.marker.remove();
-        this.showMarker(latlng);
-      } else {
-        this.showMarker(latlng);
-      };
-    });
-  }
-
-  showMarker(latLong){
-    this.marker = marker(latLong, 25);
-    this.marker.addTo(this.map)
-    .bindPopup('Hey, I\'m Here');
-    this.map.setView(latLong);
-  }
 }
--- src/app/tabHomePasajero/tabHomePasajero.page.ts
+++ src/app/tabHomePasajero/tabHomePasajero.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Map, tileLayer, marker, polyline } from "leaflet";
-import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { Map, tileLayer, marker, circle, icon} from "leaflet";
 
 @Component({
   selector: 'app-tab2',
@@ -8,47 +7,41 @@ import { Geolocation } from "@ionic-native/geolocation/ngx";
   styleUrls: ['tabHomePasajero.page.scss']
 })
 export class Tab2Page {
-
   map: Map;
   marker: any;
-  latLong = [];
+  icon: any;
+  circle: any;
+  yellowIcon = icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
-  constructor(private geolocation: Geolocation) {}
+  constructor() {}
 
   ionViewDidEnter(){
     this.showMap();
   }
 
   showMap() {
-
-    this.map = new Map("myMap").setView([40.410915, -3.708794], 55);
+    this.map = new Map("myMap").setView([40.434578, -3.698599], 55);
     tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
 
-  }
+    this.marker = marker([40.434578, -3.698599], 25).addTo(this.map).bindPopup("I am here.");;
+    this.circle = circle([40.434578, -3.698599], {
+      color: 'blue',
+      fillOpacity: 0.2,
+      radius: 75
+  }).addTo(this.map);
 
-  getPositions(){
-    this.geolocation.getCurrentPosition({
-      enableHighAccuracy: true
-    }).then((res) => {
-      return this.latLong = [
-        res.coords.latitude,
-        res.coords.longitude
-      ]
-    }).then((latlng) => {
-      if (this.marker) {
-        this.marker.remove();
-        this.showMarker(latlng);
-      } else {
-        this.showMarker(latlng);
-      };
-    });
-  }
+  this.marker = marker([40.434958, -3.698749], {icon: this.yellowIcon}).addTo(this.map);
+  this.marker = marker([40.434275, -3.698391], {icon: this.yellowIcon}).addTo(this.map);
+  this.marker = marker([40.434617, -3.698510], {icon: this.yellowIcon}).addTo(this.map);
+  this.marker = marker([40.434117, -3.698562], {icon: this.yellowIcon}).addTo(this.map);
 
-  showMarker(latLong){
-    this.marker = marker(latLong, 25);
-    this.marker.addTo(this.map)
-    .bindPopup('Hey, I\'m Here');
-    this.map.setView(latLong);
   }
 
 }
